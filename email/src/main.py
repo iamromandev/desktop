d4fdc31c29b6815bfa929e08e502cf6fa2f143b6
Roman <iamromandev@gmail.com>
2023-02-11 03:12:41 +0600

    added email clien to search all emails

--- email/src/main.py
+++ email/src/main.py
@@ -1,6 +1,14 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt6.QtWidgets import (
+    QApplication,
+    QVBoxLayout,
+    QWidget,
+    QPushButton,
+
+)
+
+from clients import EmailClient
 
 
 class Window(QWidget):
@@ -9,12 +17,20 @@ class Window(QWidget):
         self.resize(300, 300)
         self.setWindowTitle("Email Manager")
 
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
         button = QPushButton("Search Email", self)
-        button.move(100, 100)
+        button.clicked.connect(self.clicked)
+        layout.addWidget(button)
+
+    def clicked(self):
+        print("Clicked")
+        email = EmailClient()
+        email.start()
 
 
 app = QApplication(sys.argv)
 window = Window()
 window.show()
 sys.exit(app.exec())
-
